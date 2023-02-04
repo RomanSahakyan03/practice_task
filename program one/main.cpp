00c831bdcb2f3ae484dd84b3b9a1a6eea19477b5
RomanSahakyan03 <116185879+RomanSahakyan03@users.noreply.github.com>
2023-02-04 16:07:01 +0000

    some changes

--- program one/main.cpp
+++ program one/main.cpp
@@ -7,12 +7,23 @@
 #include <fcntl.h> 
 #include <unistd.h>
 #include <sys/shm.h>
+#include <sys/ipc.h>
 
 pthread_cond_t  cv;
 pthread_mutex_t mtx;
 
+struct args_t{
+  std::string *shared_str;
+  void *shared_memory;
+  
+};
+
 void *ProccessUserInput(void *args){
-    std::string *shared_buffer = static_cast<std::string *>(args);
+    
+    // Cast to struct type
+    args_t *thread_args = static_cast<args_t *>(args);
+    
+    std::string *shared_buffer = static_cast<std::string *>(thread_args->shared_str);
     while(true){
 
         // Lock the shared buffer to prevent race conditions
@@ -68,11 +79,18 @@ void *ProccessUserInput(void *args){
 }
 
 void* PrintProcessedInput(void* args) {
+    
+    // Cast to struct type
+    args_t *thread_args = static_cast<args_t *>(args);
+    
     // Cast the argument to a pointer to a string object
-    std::string *shared_buffer = static_cast<std::string *>(args);
+    std::string *shared_buffer = static_cast<std::string *>(thread_args->shared_str);
+    
     while(true) {
+        
         // Lock the mutex before accessing the shared buffer
         pthread_mutex_lock(&mtx);
+        
         while(shared_buffer->empty()) {
             // Wait on the condition variable if the buffer is empty
             pthread_cond_wait(&cv, &mtx);
@@ -80,6 +98,18 @@ void* PrintProcessedInput(void* args) {
 
         // Print the processed input string
         std::cout << "Processed input: " << *shared_buffer << std::endl;
+        
+        // Computing the sum of the digits
+        unsigned *sum = new unsigned{0};
+        for(const auto ch : *shared_buffer){
+            if(std::isdigit(ch)){
+                *sum += ch - '0';
+            }
+        }
+        std::cout << *sum << std::endl;
+        
+        // Put the sum in the shared buffer
+        thread_args->shared_memory = (void *)sum;
 
         // Clear the buffer after reading
         shared_buffer->clear();
@@ -96,11 +126,14 @@ void* PrintProcessedInput(void* args) {
 
 int main(int argc, char **argv) {
 
+    // making a shared memory for threads
+    std::string *shared_str = new std::string;
+    
     // Create a shared memory object
-     int shm_id = shmget(IPC_PRIVATE, 1024, 0644 | IPC_CREAT);
+    int shm_id = shmget(2455, 10, 0644 | IPC_CREAT);
 
     // Check if the shared memory was created successfully
-     if (shm_id == -1) {
+    if (shm_id == -1) {
         // Print an error message if the shared memory was not created
         std::cerr << "Error creating shared memory." << std::endl;
         return 1;
@@ -116,13 +149,19 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-
     // Create two threads
     pthread_t thread1, thread2;
+    
+    
+    // Create a struct with arguments for second thread
+    args_t *thread_args = new args_t;
+    thread_args->shared_str = shared_str;
+    thread_args->shared_memory = shm_ptr;
+    
 
     // Pass the shared buffer pointer to the thread functions
-    pthread_create(&thread1, NULL, ProccessUserInput, (void*) shm_ptr);
-    pthread_create(&thread2, NULL, PrintProcessedInput, (void*) shm_ptr);
+    pthread_create(&thread1, NULL, ProccessUserInput, (void*) thread_args);
+    pthread_create(&thread2, NULL, PrintProcessedInput, (void*) thread_args);
     
     // Join threads
     pthread_join(thread1, NULL);
@@ -133,6 +172,10 @@ int main(int argc, char **argv) {
 
     // Mark the shared memory for destruction
     shmctl(shm_id, IPC_RMID, NULL);
+    
+    // Deleting dynamic variables;
+    delete thread_args;
+    delete shared_str;
     
     return 0;
 }
